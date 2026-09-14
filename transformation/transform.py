a3cca7d372e423eff65e750f53c560a493172c6f
zakarya hari <zakariahari42@gmail.com>
2026-09-14 16:30:47 +0100

    Add precipitation category and points calculation to weather data transformation

--- transformation/transform.py
+++ transformation/transform.py
@@ -44,14 +44,23 @@ wind_cat_choices = ["Calm", "Moderate", "High"]
 wind_point_choices = [0, 15, 40]
 
 
+precip_conditions = [
+    (df['precipitation_sum'] == 0.0),
+    (df['precipitation_sum'] >= 0.1) & (df['precipitation_sum'] <= 5.0),
+    (df['precipitation_sum'] > 5.0)
+]
+precip_cat_choices = ["Dry", "Light Rain", "Heavy Rain"]
+precip_point_choices = [0, 20, 45]
+
+
+
 
 df['wind_category'] = np.select(wind_conditions, wind_cat_choices, default="Unknown")
-
+df['precip_category'] = np.select(precip_conditions, precip_cat_choices, default="Unknown")
 
 
 df['wind_points'] = np.select(wind_conditions, wind_point_choices, default=0)
+df['precip_points'] = np.select(precip_conditions, precip_point_choices, default=0)
 
 
-
-
-
+df.to_csv("data/gold/All_Moroccain_Citys.csv" , index=False)
