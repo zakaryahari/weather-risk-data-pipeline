47854ca13727815f5d1c6633c007d087aee16629
zakarya hari <zakariahari42@gmail.com>
2026-09-14 15:07:48 +0100

    Add data transformation logic to aggregate weather data for Moroccan cities

--- transformation/transform.py
+++ transformation/transform.py
@@ -9,3 +9,25 @@ file_paths = glob.glob("data/bronze/citys/*.json")
 
 df = pd.DataFrame()
 print(df)
+
+for path in file_paths:
+
+    file_name = os.path.basename(path) 
+
+    with open(path) as file :
+        obj = json.load(file)
+
+    city_name = file_name.replace(".json", "") 
+
+    tab = pd.DataFrame(obj['daily'])
+    tab['city'] = city_name
+    tab['latitude'] = obj['latitude']
+    tab['longitude'] = obj['longitude']
+    tab['extracted_at'] = pd.Timestamp.now()
+
+    tab.fillna(0)
+
+    df = pd.concat([df , tab] , ignore_index=True)
+
+
+df.to_csv("data/silver/All_Moroccain_Citys.csv" , index=False)
