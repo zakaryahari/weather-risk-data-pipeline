b81e6149554a1173b335b2438aa5286cc515aa64
zakarya hari <zakariahari42@gmail.com>
2026-09-14 16:31:12 +0100

    Add temperature category and points calculation to weather data transformation

--- transformation/transform.py
+++ transformation/transform.py
@@ -35,6 +35,7 @@ df.to_csv("data/silver/All_Moroccain_Citys.csv" , index=False)
 
 # print(df.head())
 
+
 wind_conditions = [
     (df['wind_speed_10m_max'] < 20),
     (df['wind_speed_10m_max'] >= 20) & (df['wind_speed_10m_max'] <= 40),
@@ -53,14 +54,24 @@ precip_cat_choices = ["Dry", "Light Rain", "Heavy Rain"]
 precip_point_choices = [0, 20, 45]
 
 
+temp_conditions = [
+    (df['temperature_2m_max'] >= 5) & (df['temperature_2m_max'] <= 35),
+    (df['temperature_2m_max'] > 35),
+    (df['temperature_2m_max'] < 5)
+]
+temp_cat_choices = ["Normal", "Extreme Heat", "Extreme Cold"]
+temp_point_choices = [0, 15, 15]
 
 
 df['wind_category'] = np.select(wind_conditions, wind_cat_choices, default="Unknown")
 df['precip_category'] = np.select(precip_conditions, precip_cat_choices, default="Unknown")
+df['temp_category'] = np.select(temp_conditions, temp_cat_choices, default="Unknown")
 
 
 df['wind_points'] = np.select(wind_conditions, wind_point_choices, default=0)
 df['precip_points'] = np.select(precip_conditions, precip_point_choices, default=0)
+df['temp_points'] = np.select(temp_conditions, temp_point_choices, default=0)
+
 
 
 df.to_csv("data/gold/All_Moroccain_Citys.csv" , index=False)
