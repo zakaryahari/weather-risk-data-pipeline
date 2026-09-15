726337bf295edee08678213e18e1c4a9f8eb8aab
zakarya hari <zakariahari42@gmail.com>
2026-09-15 16:19:57 +0100

    Add final batch processing for leftover city forecasts and save to JSON

--- extraction/extract.py
+++ extraction/extract.py
@@ -40,7 +40,22 @@ for index , row in df.iterrows() :
 
         latitude_list = []
         longitude_list = []
-        batch_number += 1
+        counter += 1
+
+    if len(latitude_list) > 0:
+    
+        lat_str = ",".join(latitude_list)
+        lng_str = ",".join(longitude_list)
+        
+        api_url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat_str + "&longitude=" + lng_str + "&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,weather_code&timezone=auto"
+        
+        print(f"Sending final leftover batch {c}...")
+        response = requests.get(api_url)
+        
+        if response.status_code == 200:
+            file_name = f"data/bronze/citys/batch_{counter}_Bronze.json"
+            with open(file_name, "w") as file:
+                json.dump(response.json(), file, indent=4)
 
     
 
