4b96414dc2027cf6ac1af249391ff92741a6f44d
zakarya hari <zakariahari42@gmail.com>
2026-09-16 16:38:34 +0100

    Refactor load script to streamline database connection and schema verification

--- load/load.py
+++ load/load.py
@@ -1,41 +1,26 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.dialects.postgresql import insert
+
+connection_db = "postgresql://admin:admin@localhost:5432/logistics_weather"
+
+engine = create_engine(connection_db)
+
+
 
 df_cities = pd.read_csv("data/maroc_cities.csv")
 df_gold = pd.read_csv("data/gold/All_Moroccain_Citys.csv")
 
 df_cities = df_cities.drop(columns=["country","iso2","admin_name","capital","population","population_proper"])
 df_cities = df_cities.rename(columns={'lat':'latitude' , 'lng' : 'longitude'})
-df_cities['city_id'] = range(1, len(df_cities) + 1)
-print(df_cities)
-
-merged_df = pd.merge(df_cities , df_gold , on="city")
-
-final_df = merged_df.drop(columns=[ "city" , "latitude_x" , "longitude_x"  , "latitude_y" , "longitude_y"])
-
-print(final_df)
-
-connection_db = "postgresql://admin:admin@localhost:5432/logistics_weather"
-
-engine = create_engine(connection_db)
-
-print(pd.read_sql())
-
-df_cities.to_sql(
-    name='cities',    
-    con=engine,              
-    if_exists='append',      
-    index=False        
-)
-
-# final_df.to_sql(
-#     name='forecasts',    
-#     con=engine,              
-#     if_exists='append',      
-#     index=False        
-# )
 
 
 
 
+with open("load/Weather_DB.session.sql", "r") as file:
+    schema_query = file.read()
+
+with engine.begin() as connection:
+    connection.execute(text(schema_query))
+    print("Database schema verified and ready!")
 
