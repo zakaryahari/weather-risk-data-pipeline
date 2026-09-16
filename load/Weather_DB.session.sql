70d935b6894a03bb92297c3f3f6b5e2ffc6cf85a
zakarya hari <zakariahari42@gmail.com>
2026-09-16 16:38:39 +0100

    Add SQL script to create cities and forecasts tables in the weather database

---
+++ load/Weather_DB.session.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS forecasts;
+DROP TABLE IF EXISTS cities;
+
+
+CREATE TABLE cities (
+    city_id SERIAL PRIMARY KEY,
+    city VARCHAR(100) UNIQUE NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT
+);
+
+
+CREATE TABLE forecasts (
+    forecast_id SERIAL PRIMARY KEY, 
+    city_id INT REFERENCES cities (city_id),
+    time DATE,
+    temperature_2m_max FLOAT,
+    precipitation_sum FLOAT,
+    wind_speed_10m_max FLOAT,
+    temp_category VARCHAR(50),
+    precip_category VARCHAR(50),
+    wind_category VARCHAR(50),
+    risk_score INT,
+    extracted_at TIMESTAMP,
+    CONSTRAINT unique_city_time UNIQUE (city_id, time)
+);
+
+SELECT * FROM cities
